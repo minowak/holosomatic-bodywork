a539d177e80bc4a85a4065759fdcae20ed0f7db3
Michal Nowak <minowak@protonmail.ch>
2024-12-09 18:54:25 +0100

    Mobile locale switcher

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -9,15 +9,42 @@ import { MenuIcon } from "lucide-react";
 export const Header: React.FC<SectionProps> = ({ lang }) => {
   const t = getTranslations(lang);
 
+  const flagStyle = {
+    fontSize: '2em',
+    lineHeight: '0em',
+  }
+
   return (
     <header className="sticky top-0 z-50 shadow-md text-primary px-8 py-4 bg-white">
-      <div className="flex justify-between gap-4">
+      <div className="flex justify-between gap-4 items-center">
         <div className="flex gap-4 items-center flex-1">
           <div><Logo className="size-8" /></div>
           <div>
             <span className="text-lg font-bold mr-2">Yuliia Pinchuk</span>
             <span className="md:inline hidden">{t.facilitator}</span>
           </div>
+        </div>
+        <menu className="md:flex gap-4 flex-1 justify-center hidden items-center">
+          <MenuItem label={t.home} anchor="home" />
+          <MenuItem label={t.services} anchor="services" />
+          <MenuItem label={t.about} anchor="about-me" />
+          <MenuItem label={t.method} anchor="about-the-method" />
+          <MenuItem label={t.contact} anchor="contact" />
+        </menu>
+        <div className="md:flex-1 text-right text-sm font-semibold">
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <ReactCountryFlag countryCode={lang === "en" ? "GB" : lang?.toUpperCase() || "UK"} className="mr-2 mb-1 md:mb-0"
+                style={flagStyle} />
+              <span className="font-bold hidden md:inline">{(t as any)[lang || "en"]}</span>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem><Link href="/en" replace><ReactCountryFlag countryCode="GB" className="mr-2" style={flagStyle} />{t.en}</Link></DropdownMenuItem>
+              <DropdownMenuItem><Link href="/pl" replace><ReactCountryFlag countryCode="PL" className="mr-2" style={flagStyle} />{t.pl}</Link></DropdownMenuItem>
+              <DropdownMenuItem><Link href="/ru" replace><ReactCountryFlag countryCode="RU" className="mr-2" style={flagStyle} />{t.ru}</Link></DropdownMenuItem>
+              <DropdownMenuItem><Link href="/ua" replace><ReactCountryFlag countryCode="UA" className="mr-2" style={flagStyle} />{t.ua}</Link></DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
         <div className="visible md:hidden cursor-pointer">
           <DropdownMenu>
@@ -33,27 +60,6 @@ export const Header: React.FC<SectionProps> = ({ lang }) => {
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
-        <menu className="md:flex gap-4 flex-1 justify-center hidden">
-          <MenuItem label={t.home} anchor="home" />
-          <MenuItem label={t.services} anchor="services" />
-          <MenuItem label={t.about} anchor="about-me" />
-          <MenuItem label={t.method} anchor="about-the-method" />
-          <MenuItem label={t.contact} anchor="contact" />
-        </menu>
-        <div className="hidden md:block flex-1 text-right text-sm font-semibold">
-          <DropdownMenu>
-            <DropdownMenuTrigger>
-              <ReactCountryFlag countryCode={lang === "en" ? "GB" : lang?.toUpperCase() || "UK"} className="mr-2" />
-              <span className="font-bold">{(t as any)[lang || "en"]}</span>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuItem><Link href="/en" replace><ReactCountryFlag countryCode="GB" className="mr-2" />{t.en}</Link></DropdownMenuItem>
-              <DropdownMenuItem><Link href="/pl" replace><ReactCountryFlag countryCode="PL" className="mr-2" />{t.pl}</Link></DropdownMenuItem>
-              <DropdownMenuItem><Link href="/ru" replace><ReactCountryFlag countryCode="RU" className="mr-2" />{t.ru}</Link></DropdownMenuItem>
-              <DropdownMenuItem><Link href="/ua" replace><ReactCountryFlag countryCode="UA" className="mr-2" />{t.ua}</Link></DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
       </div>
     </header>
   );
@@ -65,7 +71,7 @@ type MenuItemProps = {
 }
 
 const MenuItem: React.FC<MenuItemProps> = ({ label, anchor }) => {
-  return <div className="duration-150 hover:text-secondary cursor-pointer">
+  return <div className="duration-150 hover:text-secondary cursor-pointer text-center">
     <a href={"#" + anchor}>{label}</a>
   </div>
 }
